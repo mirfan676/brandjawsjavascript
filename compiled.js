d4a68c6dbef5fd7cb6e11d3b341f21080b429c0c
Muhammad Irfan <88087250+mirfan676@users.noreply.github.com>
2023-11-17 17:43:38 +0500

    Add existing project files to Git

--- compiled.js
+++ compiled.js
@@ -71,16 +71,3 @@ function getCorrectAnswer(questionNumber) {
   };
   return correctAnswers[questionNumber];
 }
-"use strict";
-
-function getCorrectAnswer(questionNumber) {
-  // Replace this with your correct answers for each question
-  var correctAnswers = {
-    "1": "1",
-    "2": "2",
-    "3": "3",
-    "4": "4",
-    "5": "5"
-  };
-  return correctAnswers[questionNumber];
-}
